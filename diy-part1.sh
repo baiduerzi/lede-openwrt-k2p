42414077be05e4df80570983dbf00309d5a50404
baiduerzi <31235783+baiduerzi@users.noreply.github.com>
2021-08-22 22:36:38 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,6 +14,6 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-git clone https://github.com/sirpdboy/luci-app-autotimeset package/luci-app-autotimeset
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo git clone https://github.com/sirpdboy/luci-app-autotimeset package/luci-app-autotimeset
